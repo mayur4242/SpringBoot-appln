1b0ea727e1d9f0371d7f8fbd0f2db900b2c0c12c
mayur4242 <khatavkarmayur4242@gmail.com>
2024-05-12 15:41:30 +0530

    Configure project

---
+++ src/main/java/Com/spring/test/SpringBootProjectApplication.java
@@ -0,0 +1,13 @@
+package Com.spring.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootProjectApplication.class, args);
+	}
+
+}
