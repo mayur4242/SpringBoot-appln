b03dd972523b5c7d1f4b68a7173214c35854ce0f
mayur4242 <khatavkarmayur4242@gmail.com>
2024-05-12 15:50:49 +0530

    Addition of 2 numbers

--- src/main/java/Com/spring/test/SpringBootProjectApplication.java
+++ src/main/java/Com/spring/test/SpringBootProjectApplication.java
@@ -10,6 +10,13 @@ public class SpringBootProjectApplication {
 		SpringApplication.run(SpringBootProjectApplication.class, args);
 
 		System.out.println("Hello World");
+		
+		int a = 10;
+		int b =10;
+		int c = a+b;
+		
+		
+		System.out.println("Addition =" + c);
 	}
 
 }
